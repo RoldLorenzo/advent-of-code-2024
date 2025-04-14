a5876a307f037506b168074ed9b9b5905af8c36d
Rold Lorenzo <lorenzo@laboware.com.br>
2025-04-14 16:27:08 -0300

    refactor day_3

--- day_3.py
+++ day_3.py
@@ -6,21 +6,18 @@ class InstructParser:
         self.position = 0
         self.active = True
 
-    def parse_only_mul(self) -> int:
-        total = 0
-        while not self.is_at_end():
-            if self.match("mul"):
-                total += self.parse_mul()
-            else:
-                self.advance()
+    def parse(self) -> tuple[int, int]:
+        part_one = 0
+        part_two = 0
         
-        return total
-
-    def parse(self) -> int:
-        total = 0
         while not self.is_at_end():
             if self.match("mul"):
-                total += self.parse_mul()
+                result = self.parse_mul()
+                
+                part_one += result
+                
+                if self.active: 
+                    part_two += result
             elif self.match("don't"):
                 self.set_active(False)
             elif self.match("do"):
@@ -28,14 +25,11 @@ class InstructParser:
             else:
                 self.advance()
         
-        return total
+        return (part_one, part_two)
     
     # With 'mul' already consumed, expect '(<number>,<number>)' and return the 2 number multiplied.
     # If the arguments are not correctly formated, or if self.active is false, return 0.  
-    def parse_mul(self) -> int:
-        if not self.active:
-            return 0
-        
+    def parse_mul(self) -> int:        
         if not self.match("("):
             return 0
         
@@ -54,10 +48,8 @@ class InstructParser:
     # With 'do' or 'don't' already consumed, expect '()'.
     # If '()' gets consumed, set self.active to value.
     def set_active(self, value: bool):
-        if not self.match("()"):
-            return
-        
-        self.active = value
+        if self.match("()"):
+            self.active = value
     
     def parse_number(self):
         number_txt = ""
@@ -80,7 +72,9 @@ class InstructParser:
     
     # Returns the next n chars without consuming them
     def peek(self, n: int) -> str:
-        if self.is_at_end(): return ""
+        if self.is_at_end(): 
+            return ""
+        
         return self.input[self.position : self.position + n]
 
     def is_at_end(self) -> bool:
@@ -90,9 +84,6 @@ if __name__ == "__main__":
     input = file_as_string()
     
     parser = InstructParser(input)
-    part_one = parser.parse_only_mul()
-    
-    parser = InstructParser(input)
-    part_two = parser.parse()
+    part_one, part_two = parser.parse()
     
     print_results(part_one, part_two)
