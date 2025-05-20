465c3bd5a758fc73bcad388c84102b8407ab4e7b
Rold Lorenzo <lorenzo@laboware.com.br>
2025-05-19 21:21:25 -0300

    refactor day_3 with regular expressions

--- day_3.py
+++ day_3.py
@@ -1,89 +1,45 @@
 from util import *
+import re
 
-class InstructParser:
-    def __init__(self, input: str):
-        self.input = input
-        self.position = 0
-        self.active = True
-
-    def parse(self) -> tuple[int, int]:
-        part_one = 0
-        part_two = 0
-        
-        while not self.is_at_end():
-            if self.match("mul"):
-                result = self.parse_mul()
-                
-                part_one += result
-                
-                if self.active: 
-                    part_two += result
-            elif self.match("don't"):
-                self.set_active(False)
-            elif self.match("do"):
-                self.set_active(True)
-            else:
-                self.advance()
-        
-        return (part_one, part_two)
+def parse_only_muls(input: str) -> int:
+    pattern = r"mul\((-?\d+),(-?\d+)\)"
+    result = 0
     
-    # With 'mul' already consumed, expect '(<number>,<number>)' and return the 2 number multiplied.
-    # If the arguments are not correctly formated, or if self.active is false, return 0.  
-    def parse_mul(self) -> int:        
-        if not self.match("("):
-            return 0
-        
-        num1 = self.parse_number()
-        
-        if not self.match(","):
-            return 0
-        
-        num2 = self.parse_number()
-        
-        if not self.match(")"):
-            return 0
-
-        return num1 * num2
-    
-    # With 'do' or 'don't' already consumed, expect '()'.
-    # If '()' gets consumed, set self.active to value.
-    def set_active(self, value: bool):
-        if self.match("()"):
-            self.active = value
-    
-    def parse_number(self):
-        number_txt = ""
-        while not self.is_at_end() and self.peek(1).isdecimal(): 
-            number_txt += self.advance()
+    for match in re.finditer(pattern, input):
+        n1 = match.group(1)
+        n2 = match.group(2)
+        if n1 and n2:
+            result += int(n1) * int(n2)
             
-        return int(number_txt)
-    
-    def advance(self):
-        self.position += 1
-        return self.input[self.position - 1]
-    
-     # Consumes the next chars if they are equal to expected
-    def match(self, expected: str) -> bool:
-        if self.is_at_end() or self.peek(len(expected)) != expected:
-            return False
-        
-        self.position += len(expected)
-        return True
-    
-    # Returns the next n chars without consuming them
-    def peek(self, n: int) -> str:
-        if self.is_at_end(): 
-            return ""
-        
-        return self.input[self.position : self.position + n]
+    return result
 
-    def is_at_end(self) -> bool:
-        return self.position >= len(self.input)
+def parse_instructions(input: str) -> int:
+    active = True
+    result = 0
+    pattern = r"""
+        mul\((-?\d+),(-?\d+)\)|
+        do\(\)|
+        don't\(\)
+    """
+    
+    for match in re.finditer(pattern, input, flags=re.VERBOSE):
+        full_match = match.group(0)
+        group_1 = match.group(1)
+        group_2 = match.group(2)
+        
+        if full_match == "do()":
+            active = True
+        elif full_match == "don't()":
+            active = False
+        elif group_1 and group_2 and active:
+            result += int(group_1) * int(group_2)
+            
+    return result
 
 if __name__ == "__main__":
     input = file_as_string()
     
-    parser = InstructParser(input)
-    part_one, part_two = parser.parse()
+    part_one = parse_only_muls(input)
+    part_two = parse_instructions(input)
     
     print_results(part_one, part_two)
