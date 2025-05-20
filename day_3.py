f63aedfdad7640d7d4dc086baa48bc8fd43289c8
Rold Lorenzo <lorenzo@laboware.com.br>
2025-05-19 21:33:32 -0300

    refactor using named groups

--- day_3.py
+++ day_3.py
@@ -1,20 +1,12 @@
 from util import *
 import re
 
-def parse_mul(match: re.Match) -> int:
-    group_1 = match.group(1)
-    group_2 = match.group(2)
-    
-    assert group_1 and group_2
-    
-    return int(group_1) * int(group_2)
-
 def parse_only_muls(input: str) -> int:
     pattern = r"mul\((-?\d+),(-?\d+)\)"
     result = 0
     
     for match in re.finditer(pattern, input):
-        result += parse_mul(match)
+        result += int(match.group(1)) * int(match.group(2))
             
     return result
 
@@ -22,24 +14,24 @@ def parse_instructions(input: str) -> int:
     active = True
     result = 0
     pattern = r"""
-        mul\((-?\d+),(-?\d+)\)|
-        do\(\)|
-        don't\(\)
+        mul\((?P<x>-?\d+),(?P<y>-?\d+)\) |
+        (?P<do>do\(\)) |
+        (?P<dont>don't\(\))
     """
     
-    for match in re.finditer(pattern, input, flags=re.VERBOSE):
-        full_match = match.group(0)
-        
-        if full_match == "do()":
-            active = True
+    for match in re.finditer(pattern, input, flags=re.VERBOSE):        
+        if match.group("do"):
+            active = True  
             continue
-        
-        if full_match == "don't()":
+              
+        if match.group("dont"):
             active = False
             continue
         
+        x = match.group("x")
+        y = match.group("y")
         if active:
-            result += parse_mul(match)
+            result += int(x) * int(y)
             
     return result
 
